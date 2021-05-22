388c8fd9597fb7de2d3a32a270ffcd69a6a1a240
Swogon <ignasbieksa@gmail.com>
2021-05-22 23:50:09 +0300

    upload

---
+++ Desktop/2.0/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/swogon/Desktop/Naujas2.0/googletest
+# Build directory: C:/Users/swogon/Desktop/Naujas2.0/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ Desktop/2.0/code/fun.hpp
@@ -0,0 +1,11 @@
+#ifndef fun_hpp
+#define fun_hpp
+
+#include "lib.hpp"
+#include "struct.hpp"
+#include "math.hpp"
+
+void sortedIsvedimas(vector<studentoSt> &studentas);
+void skaitymas(vector<studentoSt> &studentas);
+
+#endif
